50cc49eebb69abf3a6d870cc01adfa07775ce371
Aviprit Shah <avipritshah15@gmail.com>
2025-08-05 23:51:36 +0530

    added type command

--- app/main.ts
+++ app/main.ts
@@ -25,16 +25,17 @@ const rl = createInterface({
 
 // terminalFunction();
 
-rl.prompt();
+const commands = ["echo", "type"]
 
-rl.on("line", (line) => {
+
+const handleCommands = (line: string) => {
 
   line = line.trim();
 
-  if (line === "") {
-    rl.prompt();
-    return;
-  }
+  // Handle empty input
+  if (line === "") return
+
+
   // Handle the "exit 0" command to exit the terminal
   if (line === "exit 0") {
     process.exit(0);
@@ -42,18 +43,41 @@ rl.on("line", (line) => {
 
   const command = line.split(" ")[0];
 
-  // Handle echo command
-
+  // Handle "echo" command
   if(command === "echo") {
+
+    // Extract the message after "echo "
     const message = line.slice(5).trim();
+    
     console.log(message);
-    rl.prompt();
     return;
   }
 
+  // Handle "type" command
+  if(command === "type") {
+
+    // Extract the command name after "type "
+    const commandName = line.slice(5).trim();
+
+    // Check if the command exists in the predefined commands list
+    if (commands.includes(commandName)) {
+      console.log(`${commandName} is a shell builtin`);
+    } else {
+      console.log(`${commandName}: not found`);
+    }
+
+    return
+  }
+
+  console.log(`${command}: command not found`);
+}
 
 
-  console.log(`${line}: command not found`);
+rl.prompt();
+
+rl.on("line", (line) => {
+  
+  handleCommands(line);
 
   rl.prompt();
 });
