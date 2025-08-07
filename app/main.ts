5dba9cf187397d520886c33ded53870bcadb35ef
Aviprit Shah <avipritshah15@gmail.com>
2025-08-08 00:00:07 +0530

    fixed type command file finding behaviour

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import { createInterface } from "readline";
 import { existsSync } from "fs";
+import { execFileSync } from "child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -10,7 +11,9 @@ const rl = createInterface({
 
 const builtinCommands = ["echo", "exit", "type"]
 const isWindows = process.platform === "win32";
-const paths = process.env.PATH?.split(isWindows ? ";" : ":") || [];
+const ENVpath = process.env.PATH?.split(isWindows ? ";" : ":")[0] || "";
+const paths = ENVpath.length > 0 ? ENVpath.split(isWindows ? "\\" : "/") : [];
+// console.log(paths);
 
 const type = (command: string) => {
   // Check if the command exists in the predefined commands list
@@ -20,21 +23,28 @@ const type = (command: string) => {
 
   } else {
 
+    let currPath = ""
+
     // Check if the command exists in the filesystem
     for (const path of paths) {
 
       // Construct the file path based on the operating system
-      const filePath = 
-        isWindows ? 
-        `${path}\\${command}` // Use backslash for Windows paths
-        :
-        `${path}/${command}`; // Use forward slash for Unix-like paths
+      const filePath = currPath.length > 0 ? 
+        `${currPath}${isWindows ? `\\${path}\\${command}.exe` : `/${path}/${command}`}` 
+        : 
+        `${isWindows ? `${path}\\${command}.exe` : `${path}/${command}`}`;
 
+      // console.log(`Checking ${filePath}.....`);
       // Check if the file exists at the specified path
       if (existsSync(filePath)) {
         console.log(`${command}: is ${filePath}`);
         return;
       }
+
+      currPath += currPath.length > 0 ? 
+        `${isWindows ? `\\${path}` : `/${path}`}` 
+        : 
+        `${isWindows ? `${path}` : `${path}`}`;
     }
     console.log(`${command}: not found`);
   }
@@ -74,6 +84,31 @@ const handleCommands = (line: string) => {
     type(commandName);
     return
   }
+
+  // execute an exe file
+  const exeFilePath = isWindows ? `${command}.exe` : `./${command}`;
+
+  // Check if the file exists in the current directory or as an executable
+  if (existsSync(exeFilePath)) {
+    console.log(`Executing ${exeFilePath}`);
+
+    try {
+      // Execute the file synchronously and inherit stdio
+      execFileSync(exeFilePath, { stdio: "inherit" });
+
+    } catch (error) {
+      if( error instanceof Error) {
+        // Log the error message if execution fails
+        console.error(`Error executing ${exeFilePath}: ${error.message}`);
+      } else {
+        console.error(`Error executing ${exeFilePath}: ${error}`);
+      }
+    }
+
+    return;
+  }
+
+
 
   console.log(`${command}: command not found`);
 }
