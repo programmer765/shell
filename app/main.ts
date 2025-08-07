947d33ae1add302fdc3e384d76b449092fc861f7
Aviprit Shah <avipritshah15@gmail.com>
2025-08-08 02:51:03 +0530

    fixed args not passing when running exe file and made alone function for filePresent check

--- app/main.ts
+++ app/main.ts
@@ -13,8 +13,43 @@ const builtinCommands = ["echo", "exit", "type"]
 const isWindows = process.platform === "win32";
 const ENVpath = process.env.PATH?.split(isWindows ? ";" : ":")[0] || "";
 const paths = ENVpath.length > 0 ? ENVpath.split(isWindows ? "\\" : "/") : [];
-// console.log(paths);
+// console.log(ENVpath);
 
+
+
+
+// Function to check if a file exists in the system paths
+// Returns an object with a boolean indicating if the file was found and the file path
+const isFilePresent = (fileName: string) : { found: boolean, filePath: string } => {
+  let currPath = ""
+
+  // Check if the command exists in the filesystem
+  for (const path of paths) {
+
+    // Construct the file path based on the operating system
+    const filePath = currPath.length > 0 ? 
+      `${currPath}${isWindows ? `\\${path}\\${fileName}.exe` : `/${path}/${fileName}`}` 
+      : 
+      `${isWindows ? `${path}\\${fileName}.exe` : `${path}/${fileName}`}`;
+
+    // console.log(`Checking ${filePath}.....`);
+    // Check if the file exists at the specified path
+    if (existsSync(filePath))  return { found: true, filePath: filePath };
+
+    currPath += currPath.length > 0 ? 
+      `${isWindows ? `\\${path}` : `/${path}`}` 
+      : 
+      `${isWindows ? `${path}` : `${path}`}`;
+  }
+  return { found: false, filePath: "" };
+}
+
+
+
+
+// Function to handle the "type" command
+// It checks if the command is a shell builtin or if it exists in the filesystem
+// If it exists, it prints the file path; otherwise, it indicates that the command was not found
 const type = (command: string) => {
   // Check if the command exists in the predefined commands list
   if (builtinCommands.includes(command)) {
@@ -23,39 +58,30 @@ const type = (command: string) => {
 
   } else {
 
-    let currPath = ""
+    const result = isFilePresent(command);
 
-    // Check if the command exists in the filesystem
-    for (const path of paths) {
-
-      // Construct the file path based on the operating system
-      const filePath = currPath.length > 0 ? 
-        `${currPath}${isWindows ? `\\${path}\\${command}.exe` : `/${path}/${command}`}` 
-        : 
-        `${isWindows ? `${path}\\${command}.exe` : `${path}/${command}`}`;
-
-      // console.log(`Checking ${filePath}.....`);
-      // Check if the file exists at the specified path
-      if (existsSync(filePath)) {
-        console.log(`${command}: is ${filePath}`);
-        return;
-      }
-
-      currPath += currPath.length > 0 ? 
-        `${isWindows ? `\\${path}` : `/${path}`}` 
-        : 
-        `${isWindows ? `${path}` : `${path}`}`;
+    if(result.found === true) {
+      console.log(`${command}: is ${result.filePath}`);
+    } else {
+      console.log(`${command}: not found`);
     }
-    console.log(`${command}: not found`);
   }
 };
 
+
+
+
+// Function to handle commands entered in the terminal
 const handleCommands = (line: string) => {
 
   line = line.trim();
 
+
+
   // Handle empty input
   if (line === "") return
+
+
 
 
   // Handle the "exit 0" command to exit the terminal
@@ -63,7 +89,11 @@ const handleCommands = (line: string) => {
     process.exit(0);
   }
 
+
+
   const command = line.split(" ")[0];
+
+
 
   // Handle "echo" command
   if(command === "echo") {
@@ -75,6 +105,9 @@ const handleCommands = (line: string) => {
     return;
   }
 
+
+
+
   // Handle "type" command
   if(command === "type") {
 
@@ -85,35 +118,57 @@ const handleCommands = (line: string) => {
     return
   }
 
+
+
+
+  if (builtinCommands.includes(command) === false) {
   // execute an exe file
-  const exeFilePath = isWindows ? `${command}.exe` : `./${command}`;
+    // const exeFileName = isWindows ? `${command}.exe` : `./${command}`;
+    const isexeFilePresent = isFilePresent(command);
 
-  // Check if the file exists in the current directory or as an executable
-  if (existsSync(exeFilePath)) {
-    console.log(`Executing ${exeFilePath}`);
+    // Check if the file exists in the current directory or as an executable
+    if (isexeFilePresent.found === true) {
+      const exeFilePath = isexeFilePresent.filePath;
 
-    try {
-      // Execute the file synchronously and inherit stdio
-      execFileSync(exeFilePath, { stdio: "inherit" });
+      // Log the file path being executed
+      // console.log(`Executing ${exeFilePath}`);
 
-    } catch (error) {
-      if( error instanceof Error) {
-        // Log the error message if execution fails
-        console.error(`Error executing ${exeFilePath}: ${error.message}`);
-      } else {
-        console.error(`Error executing ${exeFilePath}: ${error}`);
+      try {
+
+        const args = line.split(" ").slice(1);
+
+        // Execute the file synchronously and inherit stdio`
+        execFileSync(exeFilePath, args, { stdio: "inherit" });
+
+      } catch (error) {
+        if( error instanceof Error) {
+          // Log the error message if execution fails
+          console.error(`Error executing ${exeFilePath}: ${error.message}`);
+        } else {
+          console.error(`Error executing ${exeFilePath}: ${error}`);
+        }
       }
-    }
 
-    return;
+      return;
+    }
+    else {
+      console.log(`${command}: command not found`);
+      return;
+    }
   }
 
 
-
+  // If the command is not recognized, print a "not found" message
   console.log(`${command}: command not found`);
 }
 
 
+
+
+
+
+
+// Start the readline interface and prompt for input
 rl.prompt();
 
 rl.on("line", (line) => {
